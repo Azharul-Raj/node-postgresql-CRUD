92c12e1bac65b0ca3bdad72f4619647b10908369
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-04-26 17:48:28 +0600

    basic CRUD operation done

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express=require("express");
-const {PrismaClient}=require("@prisma/client");
-const cors=require("cors")
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
 // const {Pool}=require("pg")
 
-const prisma=new PrismaClient();
-const app=express();
+const prisma = new PrismaClient();
+const app = express();
 
 app.use(cors());
-app.use(express.json())
-const port=3001;
-app.listen(port,()=>{
-    console.log(`server is running at ${port}`)
-})
+app.use(express.json());
+const port = 3001;
+app.listen(port, () => {
+  console.log(`server is running at ${port}`);
+});
 
 // create db model
 // const pool=new Pool({
@@ -44,23 +44,55 @@ app.listen(port,()=>{
 // createUserTable()
 
 //routing
-app.post('/add',async(req,res)=>{
+app.post("/add_user", async (req, res) => {
+  const data = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: data,
+    });
+
+    res.status(200).send(user);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+app.get("/get_user", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(`${error.message}`);
+  }
+});
+//update user
+app.put("/update_user/:id",async(req,res)=>{
+    const {id}=req.params;
+    const Id=parseInt(id)
     const data=req.body;
-    try {       
-            const user = await prisma.user.create({
-                data: data
-              });
-              
-        res.status(200).send(user)
-    } catch (error) {
-        res.send(error.message)
-    }
-})
-app.get("/",async(req,res)=>{
     try {
-        const users=await prisma.user.findMany()
-    res.status(200).json(users)
-    } catch (error) {
-        res.status(500).json(`${error.message}`)
-    }
+        const user = await prisma.user.update({
+          where:{
+              id:Id
+          },
+          data:data
+        });
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json(`${error.message}`);
+      }
 })
+// delete method
+app.delete("/delete_user/:id", async (req, res) => {
+    const {id}=req.params;
+    const Id=parseInt(id)
+    try {
+      const user = await prisma.user.delete({
+        where:{
+            id:Id
+        }
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(`${error.message}`);
+    }
+  });
